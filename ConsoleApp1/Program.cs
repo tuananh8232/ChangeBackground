881fb701862894a01c64ecd37213fa7957404791
Admin <Admin@DESKTOP-BI8RT4G>
2021-04-08 22:29:41 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+
+        [DllImport("user32.dll"),]
+        private static extern Int32 SystemParametersInfo(
+            UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        private static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
+        private static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+
+        static public void SetWallpaper(String path)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 0.ToString()); // 2 is stretched
+            key.SetValue(@"TileWallpaper", 0.ToString());
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+        static void Main(string[] args)
+        {
+            string photo = @"C:\Users\Admin\Downloads\a.jpg";
+            if (File.Exists(photo))
+            {
+                SetWallpaper(photo);
+                Console.WriteLine("Change");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+
+    }
+}
